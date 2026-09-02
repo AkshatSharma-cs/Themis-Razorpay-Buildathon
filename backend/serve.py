cfe5af8a73c8297126a3bf9e4b06c7958bb1e23f
Akshat Sharma <akshatsharma.chennai@gmail.com>
2026-09-02 18:01:46 +0530

    fix: allow all CORS origins for Vercel deployments

--- backend/serve.py
+++ backend/serve.py
@@ -364,7 +364,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://themis-razorpay-buildathon-tau.vercel.app/"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
