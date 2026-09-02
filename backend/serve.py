2d2c6a6b589008b26dfd635de2c73ca3210a19a0
Akshat Sharma <akshatsharma.chennai@gmail.com>
2026-09-02 17:47:44 +0530

    fixing connectivity

--- backend/serve.py
+++ backend/serve.py
@@ -364,7 +364,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["https://themis-razorpay-buildathon-tau.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
